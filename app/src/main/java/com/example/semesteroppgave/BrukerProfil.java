e4d2779ffb94b0d5158d392204e6e0b536b49f46
Xorie <60698534+xorie@users.noreply.github.com>
2020-09-16 13:38:56 +0200

    Brukerprofil og søkeresultat layout
    Signed-off-by: Xorie <60698534+Xorie@users.noreply.github.com>

---
+++ app/src/main/java/com/example/semesteroppgave/BrukerProfil.java
@@ -0,0 +1,14 @@
+package com.example.semesteroppgave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BrukerProfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bruker_profil);
+    }
+}
---
+++ app/src/main/java/com/example/semesteroppgave/SokeResultater.java
@@ -0,0 +1,14 @@
+package com.example.semesteroppgave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SokeResultater extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soke_resultater);
+    }
+}
